3dc6ad7e707902ac76efe2de92ca1dee056e9923
yanchao.h <yanchao.h@shulidata.com>
2019-08-06 18:28:19 +0800

    vue项目搭建iview

--- src/main.js
+++ src/main.js
@@ -3,7 +3,12 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import iView from 'iview';
+import 'iview/dist/styles/iview.css';
 
+
+//引用iView
+Vue.use(iView);
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
